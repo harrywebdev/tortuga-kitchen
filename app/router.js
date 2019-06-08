e567fb14b6933e166e1afdf5c20e9abb4cd842da
Harry B <harry@gethornet.com>
2019-06-08 13:54:11 +0200

    feature: Index route

--- app/router.js
+++ app/router.js
@@ -2,11 +2,12 @@ import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
 const Router = EmberRouter.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
+    location: config.locationType,
+    rootURL: config.rootURL,
 });
 
 Router.map(function() {
+    this.route('index', { path: '/' });
 });
 
 export default Router;
